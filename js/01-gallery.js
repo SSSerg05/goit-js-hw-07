78a5319c2360dd2677474481968e5f2255d67c0f
SSSerg05 <ssserg05@gmail.com>
2023-03-23 00:52:22 +0200

    add navigation
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -23,7 +23,9 @@ console.log(galleryItems);
 // basicLightbox.
 
 //** */ Разметка элемента галереи
-// Ссылка на оригинальное изображение должна храниться в data-атрибуте source на элементе <img>, и указываться в href ссылки. Не добавляй другие HTML теги или CSS классы кроме тех, что есть в этом шаблоне.
+// Ссылка на оригинальное изображение должна храниться в data-атрибуте source на элементе <img>, 
+// и указываться в href ссылки. Не добавляй другие HTML теги или CSS классы кроме тех, что есть 
+// в этом шаблоне.
 
 // <li class="gallery__item">
 //   <a class="gallery__link" href="large-image.jpg">
@@ -36,24 +38,18 @@ console.log(galleryItems);
 //   </a>
 // </li>
 
-// Обрати внимание на то, что изображение обернуто в ссылку, а значит при клике по умолчанию пользователь будет перенаправлен на другую страницу. Запрети это поведение по умолчанию.
+// Обрати внимание на то, что изображение обернуто в ссылку, а значит при клике по умолчанию пользователь 
+// будет перенаправлен на другую страницу. Запрети это поведение по умолчанию.
 
 //** */ Закрытие с клавиатуры
-// Добавь закрытие модального окна по нажатию клавиши Escape. Сделай так, чтобы прослушивание клавиатуры было только пока открыто модальное окно. У библиотеки basicLightbox есть метод для программного закрытия модального окна.
-
+// Добавь закрытие модального окна по нажатию клавиши Escape. 
+// Сделай так, чтобы прослушивание клавиатуры было только пока открыто модальное окно. 
+// У библиотеки basicLightbox есть метод для программного закрытия модального окна.
+const bigImages = [];
+let instance;
+let currentImage;
+let count = 0;
 const parent = document.querySelector('.gallery');
-
-const modalWindow = document.createElement('div');
-const instance = basicLightbox.create(modalWindow); 
-
-  // const img = document.createElement('img');
-
-  // img.classList.add('gallery__image')
-  // img.src = item.original;
-  // img.alt = item.description;
-
-  // modalWindow.append(img);
-  // return modalWindow
 
 // https://www.jsdelivr.com/package/npm/basiclightbox  
 // Events	Multiple ways to handle events.  
@@ -65,20 +61,41 @@ const chields = galleryItems.map(item => {
   const img = document.createElement('img');
 
   li.classList.add('gallery__item');
+  li.dataset.id = count;
+  count++;
+
   a.classList.add('gallery__link');
   a.href = item.original;
   img.classList.add('gallery__image');
   img.src = item.preview;
   img.alt = item.description;
+  img.loading = 'lazy';
   img.dataset.source = item.original;
 
-  a.addEventListener('click', (event) => {
+  li.addEventListener('click', (event) => {
     // прибираємо скачування картинки та відкриття нового вікна по замовчуванню
     event.preventDefault();
 
-    console.log(event.currentTarget);
-    // createModal(item).show;
-    instance.show;
+    currentImage = event.currentTarget;
+
+    // створюємо контейнер для модального вікна
+    const div = document.createElement('div');
+    div.style.position = 'relative';
+
+    const image = document.createElement('img');
+    image.src = item.original;
+    image.style.cursor = 'zoom-out';
+    image.addEventListener('click', () => instance.close())
+ 
+    const btnNext = addNavButton('next')
+    const btnPrev = addNavButton('prev')
+    
+    div.append( image, btnPrev, btnNext );
+  
+    // передаємо контейнер у обробчик Lightbox 
+    instance = basicLightbox.create(div); 
+    instance.show();
+    console.log(instance);
   })
 
   a.append(img);
@@ -89,3 +106,50 @@ const chields = galleryItems.map(item => {
 
 parent.append(...chields);
 
+
+function addNavButton(navstr) {
+  const pos = (navstr === 'next') ? 'right': 'left' ;
+  const btn = document.createElement('button')
+    
+  btn.type = 'button';
+  btn.classList.add( (navstr === 'next') ? 'btn-next': 'btn-prev')
+  
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    display: 'block',
+    'transform': 'transformX(50%)',
+    'font-size': '60px',
+    'padding': '10px',
+    'border': 'none',
+    'cursor': 'pointer',
+    'color': 'red',
+    'background-color' : 'rgba(0, 0, 0, .4)',
+  }
+  style[pos] = '20px';
+  Object.assign(btn.style, style)
+  btn.textContent = (navstr === 'next') ? '>': '<'
+
+  btn.addEventListener('click', () => {
+    const navigation = (navstr === 'next') ? nextSibling : prevSibling 
+    console.log(navigation)
+    navigation();
+  })
+
+  return btn;
+}
+
+function nextSibling() {
+  instance.close();
+  console.log('current',currentImage)
+  const nodeNext = currentImage.nextSibling
+  console.log('next',nodeNext) 
+  nodeNext.click();
+}
+function prevSibling() {
+  instance.close();
+  console.log('current',currentImage)
+  const nodePrev = currentImage.previousSibling  
+  console.log('prev',nodePrev)
+  nodePrev.click();
+}
