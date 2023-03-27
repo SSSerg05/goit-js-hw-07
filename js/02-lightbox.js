f40ea3e5a273942fb92ff4913090ce4bdc13bbc4
SSSerg05 <ssserg05@gmail.com>
2023-03-27 09:57:56 +0300

    2nd variant with sample-block

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,29 +9,45 @@ console.log(galleryItems);
 
 const parent = document.querySelector('.gallery');
 
+//** First variant
 // створюємо наповнення галереї li > a > img
+// const cards = galleryItems.map(item => {
+//   const li = document.createElement('li');
+//   const a = document.createElement('a');
+//   const img = document.createElement('img');
+
+//   li.classList.add('gallery__item');
+
+//   a.classList.add('gallery__link');
+//   a.href = item.original;
+
+//   img.classList.add('gallery__image');
+//   img.src = item.preview;
+//   img.alt = item.description;
+//   img.title = item.description;
+
+//   a.append(img);
+//   li.append(a);
+
+//   return li;
+// })
+// parent.append(...cards);
+
+//** Second variant
 const cards = galleryItems.map(item => { 
-  const li = document.createElement('li');
-  const a = document.createElement('a');
-  const img = document.createElement('img');
+  const sample = `
+    <li class="gallery__item">
+     <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image"
+          src="${item.preview}" 
+          alt="${item.description}" 
+          title="${item.description}" />
+     </a>
+    </li>`
+  return sample;
+});
 
-  li.classList.add('gallery__item');
-
-  a.classList.add('gallery__link');
-  a.href = item.original;
-
-  img.classList.add('gallery__image');
-  img.src = item.preview;
-  img.alt = item.description;
-  img.title = item.description;
-
-  a.append(img);
-  li.append(a);
-
-  return li;
-})
-
-parent.append(...cards);
+parent.insertAdjacentHTML('afterbegin', cards);
 
 let gallery = new SimpleLightbox('.gallery a');
 gallery.on('show.simplelightbox', function () {
